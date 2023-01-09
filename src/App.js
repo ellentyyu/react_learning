1344d2dbdd07fc9866ef7dd7c5efb60c5db25888
ellen <ellen_yu@pixnet.tw>
2023-01-09 15:42:27 +0800

    feat: 建立表單元件

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 function App() {
   const expenses = [
@@ -29,7 +30,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
