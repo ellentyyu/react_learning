403e22dcd9cb7ebf1f842bc45d9db4a98e8c67a3
ellen <ellen_yu@pixnet.tw>
2023-10-16 18:02:43 +0800

    feat: 初步狀態更新

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,19 +1,51 @@
+import { useState } from 'react';
 import './ExpenseForm.css';
 const ExpenseForm = () => {
+
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // })
+
+    // const titleChangeHandler = (event) => {
+    //     setUserInput((prevState) => {
+    //         return {
+    //             ...prevState,
+    //             enteredTitle: event.target.value
+    //         }
+    //     })
+    // }
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+
+    const amoutChangeHandler = (event) => {
+        
+    }
+
+    const dateChangeHandler = (event) => {
+        
+    }
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text" />
+                    <input type="text" onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type="number" min="0.01" step="0.01" onChange={amoutChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" />
+                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className='new-expense__actions'>
@@ -21,6 +53,6 @@ const ExpenseForm = () => {
             </div>
         </form>
     );
-  }
+}
   
-  export default ExpenseForm;
+export default ExpenseForm;
