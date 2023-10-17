819bca257d9529f7e590df6abf60e856d81f0a74
ellen <ellen_yu@pixnet.tw>
2023-10-17 18:16:19 +0800

    feat: submit事件接收表單資料

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -26,14 +26,26 @@ const ExpenseForm = () => {
     }
 
     const amoutChangeHandler = (event) => {
-        
+        setEnteredAmount(event.target.value);
     }
 
     const dateChangeHandler = (event) => {
-        
+        setEnteredDate(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
     }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
