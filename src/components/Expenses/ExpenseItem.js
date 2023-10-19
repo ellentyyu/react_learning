310bf0a7b123719e9b5104bd690146d14da10791
ellen <ellen_yu@pixnet.tw>
2023-10-19 12:41:41 +0800

    feat: 調整縮排

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,21 +3,21 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  function clickHandler() {
-    setTitle('qq');
-    console.log(title);
-  }
-  return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>change title</button>
-    </Card>
-  );
+    const [title, setTitle] = useState(props.title);
+    function clickHandler() {
+        setTitle('qq');
+        console.log(title);
+    }
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+            <button onClick={clickHandler}>change title</button>
+        </Card>
+    );
 }
 
 export default ExpenseItem;
