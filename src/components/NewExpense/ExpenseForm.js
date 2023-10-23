848d4e9953400ca9574b58cb4b454940522d3246
ellen <ellen_yu@pixnet.tw>
2023-10-23 13:54:32 +0800

    feat: 加入新增/取消編輯表單

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -47,6 +47,7 @@ const ExpenseForm = (props) => {
         setEnteredDate('');
         props.onSaveExpense(expenseData);
     }
+
     return (
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
@@ -80,6 +81,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button type="button" onClick={props.onCancel}>Cancel</button>
                 <button type="submit">Add Expense</button>
             </div>
         </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [isEditing, setEditing] = useState(false);
+    
+    const toggleEditingHandler = () => {
+        setEditing((prevVal) => !prevVal);
+    }
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const data = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         props.onAddExpense(data);
+        toggleEditingHandler()
     }
+
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpense={saveExpenseDataHandler} />
+            {!isEditing && <button type='button' onClick={toggleEditingHandler}>Add New Expense</button>}
+            {isEditing && (<ExpenseForm 
+                onSaveExpense={saveExpenseDataHandler}
+                onCancel={toggleEditingHandler}
+            />)}
         </div>
     );
 }
